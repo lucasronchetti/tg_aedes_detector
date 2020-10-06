59bcd1afdb02ec3d0a1e631234ba2eb171b54281
Lucas Ronchetti <ronchettilucas@gmail.com>
2020-10-05 23:16:26 -0300

    FFT + Spectrogram
    Generating spectrograms

--- aedes_detector/app/src/main/java/com/example/aedesdetector/ui/recorder/RecorderFragment.kt
+++ aedes_detector/app/src/main/java/com/example/aedesdetector/ui/recorder/RecorderFragment.kt
@@ -1,23 +1,35 @@
 package com.example.aedesdetector.ui.recorder
 
-import android.R.attr
 import android.app.Activity
+import android.content.ContentResolver
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import cafe.adriel.androidaudioconverter.AndroidAudioConverter
+import cafe.adriel.androidaudioconverter.callback.IConvertCallback
+import cafe.adriel.androidaudioconverter.model.AudioFormat
 import com.example.aedesdetector.R
+import com.example.aedesdetector.spec.MFCC
+import java.io.File
 
 
 class RecorderFragment : Fragment() {
 
     private val REQ_CODE_PICK_SOUNDFILE = 12345
     private lateinit var recorderViewModel: RecorderViewModel
+
+    //current recorded audio
+    private lateinit var currentAudio: ByteArray
+
+    //audio converter
+    private lateinit var audioConverterCallback: IConvertCallback
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -41,6 +53,18 @@ class RecorderFragment : Fragment() {
             )
         }
 
+        audioConverterCallback = object : IConvertCallback {
+            override fun onSuccess(convertedFile: File) {
+                // audio converted!
+                currentAudio = convertedFile.readBytes()
+            }
+
+            override fun onFailure(error: java.lang.Exception) {
+                Log.d("ERROR", error.toString())
+                // audio failed to convert
+            }
+        }
+
         return root
     }
 
@@ -48,8 +72,54 @@ class RecorderFragment : Fragment() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQ_CODE_PICK_SOUNDFILE && resultCode == Activity.RESULT_OK) {
             if (data != null) {
-                val uri = data.toUri(Intent.URI_INTENT_SCHEME)
+                try {
+                    val uri = data.data
+                    if (uri != null) {
+                        val contentResolver: ContentResolver = this.context!!.contentResolver
+                        val audioType = uri?.let { contentResolver.getType(it) }
+                        Log.d("FILETYPE", audioType)
+                        //convert here?
+                        //audio/mp4
+                        if (audioType == "audio/wav" || audioType == "audio/x-wav") {
+                            //supported format
+                            val audioBytes = uri?.let { contentResolver.openInputStream(it) }
+                            if (audioBytes != null) {
+                                currentAudio = audioBytes.readBytes()
+                                val mfccConvert = MFCC()
+                                val audioDoubleSample = currentAudio.toDoubleSamples()
+                                val mfccInput = mfccConvert.processSpectrogram(audioDoubleSample)
+                                Log.d("MFCC", mfccInput.toString())
+                            }
+                            else {
+                                Log.d("ERROR", "Null audio!")
+                            }
+                        }
+                        else {
+                            //unsuported file format, will try to convert
+                            val audioFile = File(uri.path)
+                            AndroidAudioConverter.with(this.context!!).setFile(audioFile).setFormat(
+                                AudioFormat.WAV).setCallback(audioConverterCallback).convert()
+
+                        }
+                    }
+                    else {
+                        //null uri
+                        Log.d("EXCEPTION", "Null URI")
+                    }
+
+                }
+                catch (e: Exception) {
+                    Log.d("EXCEPTION", e.toString())
+                }
             }
         }
     }
+
+    fun ByteArray.toDoubleSamples() = mapPairsToDoubles{ a, b ->
+        (a.toInt() and 0xFF or (b.toInt() shl 8)).toDouble()
+    }
+
+    inline fun ByteArray.mapPairsToDoubles(block: (Byte, Byte) -> Double)
+            = DoubleArray(size / 2){ i -> block(this[2 * i], this[2 * i + 1]) }
 }
+
