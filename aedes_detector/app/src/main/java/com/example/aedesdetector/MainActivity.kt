7f779a5708521d702e813b6c9c558c7e87a97899
Lucas Ronchetti <ronchettilucas@gmail.com>
2020-10-05 21:06:58 -0300

    File loader
    Loading audio from user files

--- aedes_detector/app/src/main/java/com/example/aedesdetector/MainActivity.kt
+++ aedes_detector/app/src/main/java/com/example/aedesdetector/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.aedesdetector
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import cafe.adriel.androidaudioconverter.AndroidAudioConverter
+import cafe.adriel.androidaudioconverter.callback.ILoadCallback
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,5 +25,15 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_map, R.id.navigation_recorder))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        AndroidAudioConverter.load(this, object : ILoadCallback {
+            override fun onSuccess() {
+                Log.d("INFO", "Loaded audio converter lib")
+            }
+
+            override fun onFailure(error: Exception) {
+                // FFmpeg is not supported by device
+                Log.d("INFO", "Failed to load audio converter")
+            }
+        })
     }
 }
--- aedes_detector/app/src/main/java/com/example/aedesdetector/ui/recorder/RecorderFragment.kt
+++ aedes_detector/app/src/main/java/com/example/aedesdetector/ui/recorder/RecorderFragment.kt
@@ -1,17 +1,22 @@
 package com.example.aedesdetector.ui.recorder
 
+import android.R.attr
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.aedesdetector.R
 
+
 class RecorderFragment : Fragment() {
 
+    private val REQ_CODE_PICK_SOUNDFILE = 12345
     private lateinit var recorderViewModel: RecorderViewModel
 
     override fun onCreateView(
@@ -22,10 +27,29 @@ class RecorderFragment : Fragment() {
         recorderViewModel =
                 ViewModelProviders.of(this).get(RecorderViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_recorder, container, false)
-        val textView: TextView = root.findViewById(R.id.text_recorder)
-        recorderViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val openFileButton: Button = root.findViewById(R.id.open_file_button)
+        openFileButton.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "audio/wav|audio/m4a|audio/mp3"
+            startActivityForResult(
+                Intent.createChooser(
+                    intent,
+                    getString(R.string.select_audio_file_title)
+                ),
+                REQ_CODE_PICK_SOUNDFILE
+            )
+        }
+
         return root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQ_CODE_PICK_SOUNDFILE && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                val uri = data.toUri(Intent.URI_INTENT_SCHEME)
+            }
+        }
     }
 }
